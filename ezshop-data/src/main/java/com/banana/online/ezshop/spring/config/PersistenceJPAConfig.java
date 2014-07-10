5344ff96d14c356318b1fc13b1829c57cddc348b
Sudheendra Rao <sudheendra.raok@cognizant.com>
2014-07-10 11:18:15 +0530

    Merge with other project

--- ezshop-data/src/main/java/com/banana/online/ezshop/spring/config/PersistenceJPAConfig.java
+++ ezshop-data/src/main/java/com/banana/online/ezshop/spring/config/PersistenceJPAConfig.java
@@ -1,4 +1,26 @@
 package com.banana.online.ezshop.spring.config;
+
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.google.common.base.Preconditions;
+
 @Configuration
 @EnableTransactionManagement
 @PropertySource({ "classpath:persistence-mysql.properties" })
